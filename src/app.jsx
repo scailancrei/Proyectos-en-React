339f04eae4e51954d75eec29c2afbe2b703e1b98
Juan Antonio <juanantonio_genial@hotmail.com>
2020-09-08 11:26:44 +0200

    Estado inicial de la aplicacion

---
+++ src/app.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+
+    render() {
+        return (
+            <div>
+                <h3>Bienvenido juan</h3>
+            </div>
+        )
+    }
+    
+}
+
+export default App;
